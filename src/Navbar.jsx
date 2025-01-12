186ea67438757b776c66401ad56aa72958c14479
Katyayan Borthakur <katyayanborthakur@gmail.com>
2025-01-12 22:59:22 +0530

    Updated Commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-const Navbar =(props) => 
-{
-    return(
+
+const Navbar = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <div className="Navbar">
             <div className="left-section">
-                {props.visit == '/blogfeed'? (
-                        <img src={props.image} alt="Navbar visual" className="navbar-img"/>
-                    ):null}
+                {props.visit === '/blogfeed' && (
+                    <img src={props.image} alt="Navbar visual" className="navbar-img"/>
+                )}
             </div>
-                <div className="middle-section">
-                    {props.searchbox ? (
-                        props.searchbox
-                    ) : props.image ? (
-                        <img src={props.image} alt="Navbar visual" className="navbar-image"/>
-                    ):null}
-                </div>
+            <div className="middle-section">
+                {props.searchbox ? (
+                    props.searchbox
+                ) : props.image ? (
+                    <img src={props.image} alt="Navbar visual" className="navbar-image"/>
+                ) : null}
+            </div>
             <div className="right-section">
-                <nav>
+                <button className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className="hamburger-line"></span>
+                    <span className="hamburger-line"></span>
+                    <span className="hamburger-line"></span>
+                </button>
+                <nav className={`nav-menu ${isOpen ? 'active' : ''}`}>
                     <ul>
                         <li>
-                            <NavLink to="/">Home</NavLink>
+                            <NavLink to="/" onClick={toggleMenu}>Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/contact">Contact Us</NavLink>
+                            <NavLink to="/contact" onClick={toggleMenu}>Contact Us</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about">About</NavLink>
+                            <NavLink to="/about" onClick={toggleMenu}>About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/display">BlogPage</NavLink>
+                            <NavLink to="/display" onClick={toggleMenu}>BlogPage</NavLink>
                         </li>
                         <li>
-                            <NavLink to={props.visit}>{props.linkname}</NavLink>
+                            <NavLink to={props.visit} onClick={toggleMenu}>{props.linkname}</NavLink>
                         </li>
                     </ul>
                 </nav>
             </div>
         </div>
     );
-}
+};
 
 export default Navbar;
